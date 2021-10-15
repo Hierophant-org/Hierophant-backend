fb84430636efebfa7688ab82fac8bf0cc71e6486
Ethan Pham <phamtuongthinh@gmail.com>
2021-10-15 13:41:53 -0400

    Fixed the HTTP Response

--- src/main/java/com/hierophant/controller/ImageController.java
+++ src/main/java/com/hierophant/controller/ImageController.java
@@ -1,9 +1,5 @@
 package com.hierophant.controller;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -66,22 +62,6 @@ public class ImageController {
 		final Image savedImage = imageService.insert(img);
 		System.out.println("Image Saved!");
 		return savedImage;
-		
-	}
-	@GetMapping("/getMemes")
-	public HttpResponse<String> getMemes()throws IOException, InterruptedException
-	{
-		 HttpClient client = HttpClient.newHttpClient();
-	        HttpRequest request = HttpRequest.newBuilder()
-	        		.uri(URI.create("https://api.imgflip.com/get_memes"))
-	                .build();
-
-	        HttpResponse<String> response = client.send(request,
-	                HttpResponse.BodyHandlers.ofString());
-
-	        System.out.println(response);
-		
-		return response;
 		
 	}
 }
