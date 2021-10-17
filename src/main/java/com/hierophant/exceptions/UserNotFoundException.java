8029418b07f7274ac77bcc625bf0b7e2c791476a
Ethan Pham <phamtuongthinh@gmail.com>
2021-10-17 19:22:28 -0400

    Minor changes

--- src/main/java/com/hierophant/exceptions/UserNotFoundException.java
+++ src/main/java/com/hierophant/exceptions/UserNotFoundException.java
@@ -18,12 +18,10 @@ public class UserNotFoundException extends RuntimeException{
 
 	public UserNotFoundException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public UserNotFoundException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 	
 	
